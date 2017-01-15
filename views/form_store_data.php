ab92c6b2960b0b789b585a6f2c5b32746ec2a1da
FEDAR PIASHKO <halenharper@gmail.com>
2017-01-15 13:08:31 +0300

    index page view

--- views/form_store_data.php
+++ views/form_store_data.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: fedarpiashko
- * Date: 15.01.17
- * Time: 12:42
- */
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+	<meta charset = "UTF-8">
+	<title>Add phone number</title>
+	<link rel = "stylesheet" href = "css/style.css">
+</head>
+<body>
+<div class="container">
+	<form id="store-form" action = "index.php?action=store" method="post">
+		<h4>Option 1. Add your phone number</h4>
+		<div class="form-group">
+			<label for = "phone">
+				Enter your PHONE:
+				<input type = "text" id="phone" name="phone">
+			</label>
+		</div>
+		<div class="form-group">
+			<label for = "email">
+				Enter your e-mail <sup>*</sup>:
+				<input type = "text" id="email" name="email" required>
+			</label>
+		</div>
+		<p>
+			Your will be able to retrieve your phone number later on using your e-mail.
+		</p>
+		<input type = "submit" value="Submit">
+	</form>
+</div>
+</body>
+</html>

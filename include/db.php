410c2bb538f3e3c1c76c85e2af3060e086f24095
FEDAR PIASHKO <halenharper@gmail.com>
2017-01-15 15:06:10 +0300

    utf-8 charset for db

--- include/db.php
+++ include/db.php
@@ -6,10 +6,10 @@ define('DB_PASSWORD', 'ExtaZY_123');
 define('DB_NAME', 'db_test');
 
 function db_connect() {
-	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die("Error: " . mysqli_error($connection));
-	if(!mysqli_set_charset($connection, "utf-8")){
-		printf("Error: " . mysqli_error($connection));
+	$link = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die("Error: " . mysqli_error($link));
+	if(!mysqli_set_charset($link, "utf8")){
+		printf("Error: " . mysqli_error($link));
 	}
 
-	return $connection;
+	return $link;
 }

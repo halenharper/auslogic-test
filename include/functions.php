d3c799a3fcc7ef31db3773411ae3c2610d592480
FEDAR PIASHKO <halenharper@gmail.com>
2017-01-15 15:08:34 +0300

    crytp - decrypt data

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+define('SALT', "lkjahsdfkhjajfhk3434534kuhsdfjkhfg");
+
 /**
  * Store form data
  * @param $link
@@ -13,8 +15,10 @@ function store_data($link, $phone, $email)
 	}
 
 	$phone = clear_data($phone);
+	$phone = crypt_phone($phone);
 
 	$email = clear_data($email);
+	$email = crypt($email, SALT);
 
 	$q = "INSERT INTO users (phone, email) VALUES ('%s', '%s')";
 
@@ -50,4 +54,24 @@ function clear_data($data)
 	$data = strip_tags($data);
 
 	return $data;
+}
+
+function crypt_phone($phone)
+{
+	$iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, SALT, utf8_encode($phone), MCRYPT_MODE_ECB, $iv);
+	$encrypted_string = base64_encode($encrypted_string);
+
+	return $encrypted_string;
+}
+
+function decrypt_phone($phone)
+{
+	$decrypted_string = base64_decode($phone);
+	$iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, SALT, $decrypted_string, MCRYPT_MODE_ECB, $iv);
+
+	return $decrypted_string;
 }

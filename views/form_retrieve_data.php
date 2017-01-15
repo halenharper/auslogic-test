8de39baab3f3bfbe403cec255095c2c7cd5dc542
FEDAR PIASHKO <halenharper@gmail.com>
2017-01-15 15:06:46 +0300

    retrieve page view

--- views/form_retrieve_data.php
+++ views/form_retrieve_data.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: fedarpiashko
- * Date: 15.01.17
- * Time: 12:43
- */
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+	<meta charset = "UTF-8">
+	<title>Retrieve my data</title>
+</head>
+<body>
+<div class="container">
+	<form id="store-form" action = "index.php?action=mail" method="post">
+		<h4>Option 2. Retrieve your phone number</h4>
+		<div class="form-group">
+			<label for = "email">
+				Enter your e-mail <sup>*</sup>:
+				<input type = "text" id="email" name="email" required>
+			</label>
+		</div>
+		<p>
+			The phone number will be e-mailed to you.
+		</p>
+		<input type = "submit" value="Submit">
+	</form>
+</div>
+</body>
+</html>
